a0c9b3623019de6450f58c33f8bddb331cbee2c6
jephersonnn <ericjeffrey.sch@gmail.com>
2022-09-15 17:52:46 +0800

    Add basic login and register methods.

--- app.js
+++ app.js
@@ -7,15 +7,11 @@ const mongoose = require("mongoose");
 const app = express();
 
 let port = process.env.PORT;
-if (port == null || port == ""){
+if (port == null || port == "") {
   port = 3000;
 }
 
-<<<<<<< HEAD
 mongoose.connect("mongodb://localhost:27017/userDB");
-=======
-mongoose.connect("mongodb://localhost:27017");
->>>>>>> bdfab1454d26434f9507b3930546dcd4ce524316
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -23,19 +19,75 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
+// -------------------------------USER SCHEMA-------------------------------
+const userSchema = {
+  email: String,
+  password: String
+}
 
+const User = mongoose.model("User", userSchema);
+// -------------------------------USER SCHEMA-------------------------------
+
+// ----------------------------------HOME-----------------------------------
 app.get("/", function(req, res) {
   res.render("home.ejs");
 })
+// ----------------------------------HOME-----------------------------------
 
+
+
+// ---------------------------------LOGIN-----------------------------------
 app.get("/login", function(req, res) {
   res.render("login.ejs");
 })
 
+app.post("/login", function(req, res) {
+  User.findOne({
+    email: req.body.username,
+  }, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === req.body.password) {
+          res.render("secrets.ejs");
+          console.log("Logged in successfully");
+        }
+      }
+    }
+  })
+})
+// ---------------------------------LOGIN-----------------------------------
+
+
+
+
+// --------------------------------REGISTER---------------------------------
 app.get("/register", function(req, res) {
   res.render("register.ejs");
 })
 
-app.listen(port, function(req,res){
-  console.log("Server started on port " + port +".");
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets.ejs");
+        console.log("Registered and logged-in successfully");
+    }
+  })
 })
+// --------------------------------REGISTER---------------------------------
+
+
+
+// --------------------------------GENERAL----------------------------------
+app.listen(port, function(req, res) {
+  console.log("Server started on port " + port + ".");
+})
+// --------------------------------GENERAL----------------------------------
